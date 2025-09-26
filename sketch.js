481a1821d0621218dc07147d12eaeb532f728690
stannardtyler <tstannard64@gmail.com>
2025-09-25 17:43:11 -0700

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -6,7 +6,7 @@ function draw() {
   background(0);
 
   lollipop(mouseX, 200, 150, 100);
-  lollipop(100, 300, 75, 25);
+  lollipop(100, mouseY, 75, 25);
   lollipop(500, 100, 500, 300);
   //lollipop(x, y, length of the stick, size of the lolli);
 }
